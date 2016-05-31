b03632999ec3bd9baf6ed09ebb86ee4d93a38e36
Guillaume Briday <guillaumebriday@gmail.com>
2016-05-31 21:33:30 +0200

    Ajout d'un template loop.php

---
+++ loop.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The loop template file.
+ *
+ * @package Minium
+ */
+?>
+
+<?php if ( have_posts() ) : ?>
+
+    <?php $count = 0; ?>
+
+    <?php /* Start the Loop */ ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <?php if ( $count % 3 == 1 ) : ?>
+            <div class="row">
+        <?php endif; ?>
+
+        <?php get_template_part( 'templates/content', get_post_format() ); ?>
+
+        <?php if ( $count % 3 == 2 || $count + 1 == count( $posts ) ) : ?>
+            </div>
+        <?php endif; ?>
+
+        <?php $count ++; ?>
+
+    <?php endwhile; ?>
+
+    <?php minium_paging_nav(); ?>
+
+<?php else : ?>
+
+    <?php get_template_part( 'templates/content', 'none' ); ?>
+
+<?php endif; ?>
